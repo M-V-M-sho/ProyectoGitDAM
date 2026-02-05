f3a52153170f2c3e99ed17a08768da385bebabd4
marvermur2 <marvermur2@pc14-info1>
2026-02-05 19:24:56 +0100

    Segundo commit: creación de clase PRUEBA1_1

---
+++ src/PRUEBA1/PRUEBA1_1.java
@@ -0,0 +1,13 @@
+package PRUEBA1;
+import java.util.*;
+
+public class PRUEBA1_1 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner (System.in);
+		System.out.println("¿Qué quieres que escriba triplicado?");
+		String copiar = s.nextLine();
+		System.out.println(copiar + copiar + copiar);
+	}
+
+}
